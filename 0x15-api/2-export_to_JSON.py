1876d94e784b4d57a26c3781658e6f9589aa9a79
Mohamed Elfakiri <elfakiri2021@gmail.com>
2024-03-27 23:43:59 +0000

    Updating 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""Accessing a R of employees"""
+""" This python script exports all data in json file """
 
 import json
 import requests
 import sys
 
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    userId = sys.argv[1]
+    user = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}"
+            .format(userId))
 
-    response = requests.get(url)
-    username = response.json().get('username')
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos")
+    todos = todos.json()
+    userdict = {}
+    tasks = []
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskdict = {
+                    "task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": user.json().get('username')}
+            tasks.append(taskdict)
+    userdict[userId] = tasks
 
-    dictionary = {employeeId: []}
-    for task in tasks:
-        dictionary[employeeId].append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        })
-    with open('{}.json'.format(employeeId), 'w') as filename:
-        json.dump(dictionary, filename)
+    filename = userId + ".json"
+    with open(filename, 'w') as jsonf:
+        json.dump(userdict, jsonf)
